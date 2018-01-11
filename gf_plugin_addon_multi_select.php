2038d0d7851abe08079d0629c489c6df565d01ec
Nick M <onethousandmarkers@gmail.com>
2018-01-11 15:28:38 -0500

    styles added

--- gf_plugin_addon_multi_select.php
+++ gf_plugin_addon_multi_select.php
@@ -7,8 +7,12 @@
  * Text Domain: gfpa-Gravity-Forms-Plugin-Addon
  */
 
+
+ 
 require_once plugin_dir_path(__FILE__) . 'src/gfpa_selectall_js.php';
 require_once plugin_dir_path(__FILE__) . 'src/gfpa_selectall_init.php';
+
+wp_enqueue_style( 'style', plugin_dir_url( __FILE__ ) . 'css/style.css', false, '1.0.0' );
 
 add_action('wp_footer', 'gfpa_addon_footer_init_call');
 
