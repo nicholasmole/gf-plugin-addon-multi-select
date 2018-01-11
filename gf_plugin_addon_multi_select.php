64f11cab4e2accb5dfc6c77103b8aae520d3aea6
Nick M <onethousandmarkers@gmail.com>
2018-01-11 12:35:28 -0500

    Version with only select all functionality only

--- gf_plugin_addon_multi_select.php
+++ gf_plugin_addon_multi_select.php
@@ -23,24 +23,23 @@ function gfpa_this_run_javascript(){
   <script>
   (function( $ ){
     $(document).ready(function(){
-    /*---------
-    Copy This Code
-    ---------*/
-    // Change the .checkall to class selector
-    //Change value="selectall" to checkbox value
-    $('.checkall li:first-child input:checkbox[value="selectall"]').click(function() {
-      // This finds :checkbox and checks all 
-      // Edit $(this) to class of box if not in the same section
-      $(this).parent('li').parent('ul').find(':checkbox').attr('checked', this.checked);
+      /*---------
+      Copy This Code
+      ---------*/
+      // Change the .checkall to class selector
+      //Change value="selectall" to checkbox value
+      //$('.checkall li:first-child input:checkbox[value="selectall"]').click(function() {
+      $('.checkall li input:checkbox[value="selectall"]').click(function() {
+        // This finds :checkbox and checks all 
+        // Edit $(this) to class of box if not in the same section
+        $('.checkallhere').find(':checkbox').attr('checked', this.checked);
+
       });
+      /* --- END COPY THIS CODE --- */
+      
     });
-    /* --- END COPY THIS CODE --- */
-    /*---------
-    EDIT CODE HERE FOR SITUATIONAL USAGE
-    ---------*/
-
-
-    /* --- END EXTRA CODE --- */
+    
+   
 
     $.fn.myfunction = function() {
       return this;
